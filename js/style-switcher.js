36a95d627037d9852c1b5d6768a54e06c18316cb
Filipe Tielson <filipe_thielsom@hotmail.com>
2023-01-07 21:11:54 -0300

    deploy

---
+++ js/style-switcher.js
@@ -0,0 +1,41 @@
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler")
+
+window.addEventListener("scroll", (event) => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open")
+    }
+})
+
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+
+
+const alternateStyles = document.querySelectorAll(".alternate-style")
+
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled")
+        }
+        else {
+            style.setAttribute("disabled", true)
+        }
+    })
+}
+
+const dayNigth = document.querySelector(".day-nigth")
+dayNigth.addEventListener("click", () => {
+    dayNigth.querySelector("i").classList.toggle("fa-sun")
+    dayNigth.querySelector("i").classList.toggle("fa-moon")
+    document.body.classList.toggle("dark")
+})
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
+        dayNigth.querySelector("i").classList.add("fa-sun")
+    }
+    else {
+        dayNigth.querySelector("i").classList.add("fa-moon")
+    }
+})
